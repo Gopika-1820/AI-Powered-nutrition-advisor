f3f4c03d476bd5ef500d3e3920a3c8b212264ed5
Gopika-1820 <gopika.2401043@srec.ac.in>
2025-09-15 11:09:28 +0530

    updated hlo file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Java from VS Code!");
+        System.out.println("I am gopika from cse");
     }
 }
